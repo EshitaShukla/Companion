858cf823f9b74600f184128e383aa8d2c95b840f
Eshita Shukla <32108487+EshitaShukla@users.noreply.github.com>
2018-06-01 19:51:23 +0530

    companion: add server side script
    and a snippet for sending request.
    
    TODO : 
    
    Update the GUI to send requests to the web server accordingly

---
+++ web.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from flask import Flask, render_template, url_for, request, session, redirect, send_from_directory, jsonify
+import os
+import serial
+import time
+
+ArduinoSerial = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+
+app = Flask(__name__)
+
+@app.route('/control')
+def control():
+    json_form = json.loads(request.json)
+    data = str(json_form['data'])
+    ArduinoSerial.write(bytes(data))
+    return 'ok'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
