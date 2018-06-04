3fba0a115cb89ea00b822356eb280c300444773f
Eshita Shukla <32108487+EshitaShukla@users.noreply.github.com>
2018-06-04 22:57:15 +0530

    Update send_request.py

--- send_request.py
+++ send_request.py
@@ -4,7 +4,7 @@ import time
 
 while True:
     actuation_value = {'data' : '4'}
-    data_json = json.dumps(sensor_values)
+    data_json = json.dumps(actuation_values)
     payload = data_json
     print(data_json)
     r = requests.get('http://host:port/route', json=payload)
