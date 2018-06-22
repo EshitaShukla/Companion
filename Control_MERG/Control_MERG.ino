f0228940fce174b77cbd57d1d077257977192093
EshitaShukla <eshitapshukla@icloud.com>
2018-06-22 10:26:51 +0530

    companion: remove Control_MERG2

--- Control_MERG/Control_MERG.ino
+++ Control_MERG/Control_MERG.ino
@@ -1,30 +1,21 @@
-/*
-  LED Flick
-  Welcome to robotics
-  
-  By Society of Robotics and Automation, VJTI
-*/
+#include <Servo.h>
 
-/** Put your setup code here, to run once 
- *  setup() is called automatically once every time bot is reset or started.
- *  It is usually used to do all initialization.
- */
-
+Servo myservo;
+int pos = 0;  
+char data;
 char data;
 
-void setup() {          
-  //Port Initialization - DDRX controls the input/output status of port X. 
+void setup() {
+  
   Serial.begin(9600);
-  DDRA  = 0b11111100;     //C7 C6 C5 C4 C3 C2 are declared as output and C1 C0 are declared as input.
-  DDRC  = 0b11111100;     //B0 B1 are declared as output and B7 B6 B5 B4 B3 B2 are declared as input.
+  myservo.attach(PB3);
+  Serial.begin(9600);
+  DDRA  = 0b11111100;     
+  DDRC  = 0b11111100;     
   DDRD = 0b11110000;
   PORTD = 0b11111111;
 }
 
-/** Put your main code here which you want to run repeatedly
- *  loop() is function which is called again and again infinitely.
- *  It is called automatically after the setup() function finishes execution.
- */
 void loop() {
   
   if (Serial.available()> 0)
@@ -32,21 +23,21 @@ void loop() {
     data = Serial.read();
   }
     //Forward
-   if (data == '1')
+   if (data == '0')
     {
       PORTC = 0b01010000;
       PORTA = 0b01010000;
     }
 
    //Backward
-   else if (data == '0')
+   else if (data == '1')
     {
       PORTC = 0b10100000;
       PORTA = 0b10100000; 
     }
 
     //Right
-   else if (data == '4')
+   else if (data == '2')
     {
       PORTC = 0b00010000;
       PORTA = 0b00010000;  
@@ -58,12 +49,31 @@ void loop() {
       PORTC = 0b01000000;
       PORTA = 0b01000000; 
     }
-
-    //quit
-    else if (data == '5')
+    
+     //EXIT
+    else if (data == '7')
     {
        PORTC = 0b00000000;
        PORTA = 0b00000000;
     }
     
+    //Give Food
+     else if (data == '4')
+    {
+      for (pos = 0; pos <= 90; pos += 1)  
+  {
+    myservo.write(pos);             
+    delay(15);                      
+  }
+    }
+
+    //Close Flap
+     else if (data == '5')
+    {
+      for (pos = 90; pos >= 0; pos -= 1)  
+  {
+    myservo.write(pos);             
+    delay(15);                      
+  }
+    }    
 }
